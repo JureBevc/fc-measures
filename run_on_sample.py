bf753f5727cf703a6568e672880854f50f10e719
JureBevc <bevc.jure@gmail.com>
2021-09-28 20:03:42 +0200

    Running metrics on actual samples

---
+++ run_on_sample.py
@@ -0,0 +1,44 @@
+import sys
+import numpy as np
+import matlab.engine
+from measures import covariance, icov, mutual_information, partial_correlation, pearson
+
+try:
+    SAMPLE_NUM = int(sys.argv[1])
+except Exception as e:
+    print("Invalid sample number")
+
+print(f"Running with input sample {SAMPLE_NUM}")
+
+# Read dataset
+eng = matlab.engine.start_matlab()
+content = eng.load("tests/data/HCP_rs_ts_bold1_Glasser.mat", nargout=1)
+
+# Get sample and apply usevec
+timeseries = np.array(content["data"]["bold1"]["data"]["timeseries"][SAMPLE_NUM])
+usevec = np.array(content["data"]["bold1"]["data"]["usevec"][SAMPLE_NUM])[0]
+usable_series = timeseries[:, usevec == 1]
+
+
+print(f"Input size: {usable_series.shape}")
+
+# Run metrics
+print("\nPearson correlation")
+R = pearson.pearson(usable_series)
+print(R)
+
+print("\nCovariance")
+R = covariance.covariance(usable_series)
+print(R)
+
+print("\nMutual information")
+R = mutual_information.mutual_information(usable_series)
+print(R)
+
+print("\nPartial correlation")
+R = partial_correlation.partial_correlation(usable_series)
+print(R)
+
+print("\nICOV")
+R = icov.ICOV(usable_series)
+print(R)
--- run_tests.py
+++ run_tests.py
@@ -1,18 +1,6 @@
-import numpy as np
-import matlab.engine
-from datetime import time
 from tests import correlation_test, covariance_test, mutual_information_test, partial_correlation_test
 from tests import icov_tests
 
-
-#eng = matlab.engine.start_matlab()
-#content = eng.load("tests/data/HCP_rs_ts_bold1_Glasser.mat", nargout=1)
-#print(content["data"]["bold1"]["data"].keys())
-
-#timeseries = np.array(content["data"]["bold1"]["data"]["timeseries"])
-#usevec = np.array(content["data"]["bold1"]["data"]["usevec"])
-#print(timeseries.shape)
-#exit()
 
 correlation_test.run_correlation_test()
 covariance_test.run_covariance_test()
